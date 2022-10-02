ac9625348a845faf4d7c0a9e24134d2031c03ba0
Michael Poletaew <poletaew@gmail.com>
2022-10-02 19:48:52 +0400

    Fixed slot limits

---
+++ slot.js
@@ -0,0 +1,93 @@
+/**
+ * Slot.js
+ * @requires jQuery 1.11
+ * Created by Michael Poletaew <poletaew@gmail.com>
+ * at 18:33, 02.10.2022 GMT+4
+ */
+
+const Slot = function (el, max, step) {
+    this.speed = 0;
+    this.step = step;
+    this.isMoving = false;
+    this.el = el;
+    this.maxSpeed = max;
+    this.minTop = 0;
+    this.maxTop = 0;
+
+    var self = this;
+
+    this.onStop = function () {
+        //on stop callback
+        return;
+    }
+
+    this.start = function () {
+        this.minTop = -.5 * $(this.el).parent().outerHeight();
+        this.maxTop = -1 * $(this.el).outerHeight() + this.minTop;
+
+        console.log('el', $(this.el), 'parent', $(this.el).parent());
+        console.log('[WM] Slot', this);
+
+        $(this.el).css('marginTop', this.minTop);
+        this.isMoving = true;
+
+        this.move();
+    };
+
+    this.correctMarginTop = function (marginTop) {
+        if (marginTop > this.minTop) marginTop = this.maxTop;
+        if (marginTop < this.maxTop) marginTop = this.minTop;
+
+        return marginTop;
+    }
+
+    this.move = function () {
+        if (this.speed < this.maxSpeed) {
+            this.speed += this.step;
+        }
+
+        let $el = $(this.el),
+            marginTop = this.correctMarginTop(this.speed + parseInt($el.css('margin-top')));
+
+        $el.animate({marginTop: marginTop}, 90, function () {
+            if (self.isMoving) {
+                setTimeout(function () {
+                    self.move();
+                }, 0);
+            }
+        });
+    };
+
+    this.slow = function () {
+        if (this.speed > 0) {
+            this.speed -= this.step;
+        } else {
+            this.onStop(this);
+            return;
+        }
+
+        var $el = $(this.el),
+            marginTop = this.correctMarginTop(this.speed + parseInt($el.css('margin-top'))),
+            remainder = marginTop % 6;
+        if (remainder) marginTop -= remainder;
+
+        console.log('this.slow marginTop', marginTop);
+
+        $el.animate({marginTop: marginTop}, 90, function () {
+            setTimeout(function () {
+                self.slow();
+            }, 0);
+        });
+
+
+    };
+
+    this.stop = function (callback) {
+        this.isMoving = false;
+        if (callback) {
+            this.onStop = callback;
+        }
+
+        this.slow();
+    };
+}
--- win-machine.js
+++ win-machine.js
@@ -1,4 +1,4 @@
-var WM = function (data) {
+const WM = function (data) {
 	this.VKdomain = 'https://vk.com/';
 	this.apiVK = "https://api.vk.com/method/";
 
@@ -33,12 +33,11 @@ var WM = function (data) {
 			this.otherGroups = $.isArray(data['other-group']) ? data['other-group'] : [data['other-group']];
 		}
 
-		var res = this.link.match(/wall((\-?\d+)_(\d+))/i);
+		let res = this.link.match(/wall((\-?\d+)_(\d+))/i);
 		if (res === null) {
 			alert('Неверная ссылка на конкурсный пост!');
 			throw new Error('Bad link');
-		}
-		else {
+		} else {
 			this.postId = res[1];
 			this.ownerId = res[2];
 			this.itemId = res[3];
@@ -203,18 +202,19 @@ var WM = function (data) {
 	this.drawSlotMachine = function () {
 		this.setStatus('Начинаем вращение...');
 
-		$('.slot').removeClass('hidden');
+		const $slot = $('.slot');
 
-		var slots = [],
+		$slot.removeClass('hidden');
+
+		let slots = [],
 			i = 0,
-			timeout = 0,
-			step = Math.ceil(Math.random() * 10 + 1);
+			timeout = 0;
 
-		$('.slot').each(function () {
+		$slot.each(function () {
 			slots[i] = new Slot(
 				$(this).find('div'),
-				step * 10,
-				step
+				Math.random() * 150 + 100,
+				Math.random() * 10 + 1
 			);
 
 			var currentSlot = slots[i];
@@ -249,8 +249,7 @@ var WM = function (data) {
 
 				if (self.otherGroups.length) {
 					self.checkWinnerGroup($(this), (currentSlot.i + 1));
-				}
-				else {
+				} else {
 					hasWinner = true;
 					self.setWinner($(this), (currentSlot.i + 1));
 				}
@@ -265,8 +264,7 @@ var WM = function (data) {
 	}
 
 	this.setWinner = function ($img, slotNumber) {
-		var id = $img.attr('id'),
-			data = $img.data(),
+		let data = $img.data(),
 			$winner = $('<div>').addClass('winner');
 
 		this.setStatus('<span class="green">Победитель # ' + slotNumber + ': @' + data.uri + ' (<b>' + data.name + '</b>)</span>');
@@ -291,10 +289,10 @@ var WM = function (data) {
 			totalWinners += parseInt(this.countWinners[i]);
 		}
 
-		if (totalWinners == $winners.length) {
+		if (totalWinners === $winners.length) {
 			var $infoDiv = $('<div>');
 
-			this.setStatus('<span class="green">Поздравляем победител' + (totalWinners == 1 ? 'я' : 'ей') + '!</span>');
+			this.setStatus('<span class="green">Поздравляем победител' + (totalWinners === 1 ? 'я' : 'ей') + '!</span>');
 
 			$('.old-statuses').prepend([$infoDiv, '<br>']);
 
@@ -306,8 +304,7 @@ var WM = function (data) {
 					+ ' (<b><a target="_blank" href="' + self.VKdomain + data.uri + '">' + data.name + '</a></b>)</div>'
 				)
 			});
-		}
-		else {
+		} else {
 			this.finalTimer = setTimeout(function () {
 				self.showFinalInfo();
 			}, 700);
@@ -317,7 +314,7 @@ var WM = function (data) {
 	this.checkWinnerGroup = function ($img, slotNumber) {
 		var $winnerWaiting = $('<div>').addClass('winner waiting'),
 			neededGroups = []
-			;
+		;
 
 		for (var i in this.otherGroups) {
 			var tmp = this.otherGroups[i].match(/vk.com\/([^?]+)\??.*$/);
@@ -328,8 +325,7 @@ var WM = function (data) {
 
 		if (neededGroups.length > 1) {
 			this.checkMultiGroupsMembership($winnerWaiting, $img, neededGroups);
-		}
-		else {
+		} else {
 			this.checkMembership($winnerWaiting, $img, neededGroups[0], slotNumber);
 		}
 	}
@@ -343,14 +339,13 @@ var WM = function (data) {
 				user_id: id
 			},
 			function (data) {
-				if (data.response == 0) {
+				if (data.response === 0) {
 					self.setStatus('<span class="red">Победитель # ' + slotNumber
 						+ ' (<a target="_blank" href="' + self.VKdomain + userInfo.uri + '">@' + userInfo.uri
 						+ '</a>) отклонён, причина: <b>не в группе</b></span>');
 
 					$object.removeClass('waiting').addClass('rejected');
-				}
-				else if (data.response == 1) {
+				} else if (data.response === 1) {
 					self.setStatus('Победитель # ' + slotNumber + ' состоит в указанной группе');
 					$object.remove();
 					self.setWinner($img, slotNumber);
@@ -370,45 +365,42 @@ var WM = function (data) {
 			},
 			function (data) {
 
-			if (data.response) {
-				var groups = data.response;
+				if (data.response) {
+					var groups = data.response;
 
-				for (var i in groups) {
-					if ($.inArray(groups[i].screen_name, neededGroups) !== -1) {
-						foundGroups.push(groups[i].screen_name);
+					for (var i in groups) {
+						if ($.inArray(groups[i].screen_name, neededGroups) !== -1) {
+							foundGroups.push(groups[i].screen_name);
+						}
 					}
-				}
 
-				if (foundGroups.length != neededGroups.length) {
+					if (foundGroups.length !== neededGroups.length) {
+						self.setStatus('<span class="red">Победитель # ' + slotNumber
+							+ ' (<a target="_blank" href="' + self.VKdomain + userInfo.uri + '">@' + userInfo.uri
+							+ '</a>) отклонён, причина: <b>не в группе</b></span>');
+
+						$object.removeClass('waiting').addClass('rejected');
+					} else {
+						self.setStatus('Победитель # ' + slotNumber + ' состоит в указанной группе');
+						$object.remove();
+						self.setWinner($img, slotNumber);
+					}
+
+				} else if (data.error) {
 					self.setStatus('<span class="red">Победитель # ' + slotNumber
 						+ ' (<a target="_blank" href="' + self.VKdomain + userInfo.uri + '">@' + userInfo.uri
-						+ '</a>) отклонён, причина: <b>не в группе</b></span>');
+						+ '</a>) отклонён, причина: <b>пользователь запретил просматривать свои группы</b></span>');
 
 					$object.removeClass('waiting').addClass('rejected');
 				}
-				else {
-					self.setStatus('Победитель # ' + slotNumber + ' состоит в указанной группе');
-					$object.remove();
-					self.setWinner($img, slotNumber);
-				}
-
 			}
-			else if (data.error) {
-				self.setStatus('<span class="red">Победитель # ' + slotNumber
-					+ ' (<a target="_blank" href="' + self.VKdomain + userInfo.uri + '">@' + userInfo.uri
-					+ '</a>) отклонён, причина: <b>пользователь запретил просматривать свои группы</b></span>');
-
-				$object.removeClass('waiting').addClass('rejected');
-			}
-		}
 		);
 	}
 
 	this.apiCall = function (method, data, callback) {
 		if (window.useSDK) {
 			VK.api(method, data, callback);
-		}
-		else {
+		} else {
 			$.get(this.apiVK + method, data, callback, "jsonp");
 		}
 	}
@@ -416,20 +408,20 @@ var WM = function (data) {
 	this.setStatus = function (text) {
 		$('.info:hidden').removeClass('hidden');
 
-		if ($('.status-text').text()) {
-			$('.old-statuses').prepend($('<div>', {html: '[WM] ' + $('.status-text').html()}));
+		const $statusText = $('.status-text');
+
+		if ($statusText.text()) {
+			$('.old-statuses').prepend($('<div>', {html: '[WM] ' + $statusText.html()}));
 		}
 
 
-		$('.status-text').html(text);
-		console.info('[WM] ' + $('.status-text').text());
+		$statusText.html(text);
+		console.info('[WM] ' + $statusText.text());
 	}
-
-
 };
 
 WM.setSlots = function (numberOfSlots) {
-	var $machine = $('.win-machine'),
+	let $machine = $('.win-machine'),
 		slots = [];
 
 	$machine.find('div').remove();
@@ -437,9 +429,9 @@ WM.setSlots = function (numberOfSlots) {
 	$machine.append($('<div>', {class: 'slot-left'}));
 
 
-	for (var i = 1; i <= numberOfSlots; i++) {
+	for (let i = 1; i <= numberOfSlots; i++) {
 		if (i > 1) {
-			$('.win-machine').append($('<div>', {class: 'slot-connector'}));
+			$machine.append($('<div>', {class: 'slot-connector'}));
 		}
 		$machine.append($('<div>', {class: 'slot-mid'}));
 		slots.push($('<div>', {
@@ -462,82 +454,4 @@ WM.setSlots = function (numberOfSlots) {
 	})
 
 	$machine.width(neWidth);
-}
-
-
-var Slot = function (el, max, step) {
-	this.speed = 0;
-	this.step = step;
-	this.isMoving = false;
-	this.el = el;
-	this.maxSpeed = max;
-	this.minTop;
-
-	var self = this;
-
-	this.onStop = function () {
-		//on stop callback
-		return;
-	}
-
-	this.start = function () {
-		this.minTop = -1 * $(this.el).outerHeight(true) + $(this.el).parent().height();
-
-		console.log('[WM] Slot', this);
-
-		$(this.el).css('marginTop', this.minTop);
-		this.isMoving = true;
-
-		this.move();
-	};
-
-	this.move = function () {
-		if (this.speed < this.maxSpeed) {
-			this.speed += this.step;
-		}
-
-		var $el = $(this.el),
-			marginTop = this.speed + parseInt($el.css('margin-top'));
-
-		if (marginTop >= 0) marginTop = this.minTop;
-
-		$el.animate({marginTop: marginTop}, 90, function () {
-			if (self.isMoving) {
-				setTimeout(function () {
-					self.move();
-				}, 0);
-			}
-		});
-	};
-
-	this.slow = function () {
-		if (this.speed > 0) {
-			this.speed -= this.step;
-		} else {
-			this.onStop(this);
-			return;
-		}
-
-		var $el = $(this.el),
-			marginTop = this.speed + parseInt($el.css('margin-top')),
-			remainder = marginTop % 6;
-		if (remainder) marginTop -= remainder;
-		if (marginTop >= 0) marginTop = this.minTop;
-		$el.animate({marginTop: marginTop}, 90, function () {
-			setTimeout(function () {
-				self.slow();
-			}, 0);
-		});
-
-
-	};
-
-	this.stop = function (callback) {
-		this.isMoving = false;
-		if (callback) {
-			this.onStop = callback;
-		}
-
-		this.slow();
-	};
 }
